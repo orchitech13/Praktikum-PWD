23a49aa8e3e1906d891db6f15e9cc41a963ca367
Orchidia Angelica Puspa Nirmala <88343877+orchitech13@users.noreply.github.com>
2021-10-06 17:19:37 +0700

    Praktikum 1
    Hit counter, guest book, upload-download file

---
+++ prak1/file/download.php
@@ -0,0 +1,10 @@
+<?php
+    $myDir = "c:/xampp/htdocs/PWD/prak1/file/";
+    $dir = opendir($myDir);
+    
+    echo "Isi folder (klik link untuk download : <br>";
+    while($tmp = readdir($dir)){
+        echo "<a href='$tmp' target='_blank'>$tmp</a><br>";
+    }
+    closedir($dir);
+?>
