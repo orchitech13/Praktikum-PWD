86ee2bde568a687bf2b109869a66b79dbd795fec
Orchidia Angelica Puspa Nirmala <88343877+orchitech13@users.noreply.github.com>
2021-10-14 22:57:04 +0700

    Add files via upload

---
+++ prak2/postest 2/akademik.sql
@@ -0,0 +1,63 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 14, 2021 at 05:54 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `akademik`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `nim` varchar(5) NOT NULL,
+  `nama` varchar(50) DEFAULT NULL,
+  `jkel` varchar(1) DEFAULT NULL,
+  `alamat` text DEFAULT NULL,
+  `tgllhr` date DEFAULT NULL,
+  `prodi` varchar(25) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`nim`, `nama`, `jkel`, `alamat`, `tgllhr`, `prodi`) VALUES
+('MHS01', 'Siti Aminah', 'P', 'SOLO', '1995-10-01', 'Teknik Informatika'),
+('MHS02', 'Rita', 'P', 'SOLO', '1999-01-01', 'Teknik Elektro'),
+('MHS03', 'Amirudin', 'L', 'SEMARANG', '1998-08-11', 'Teknik Industri'),
+('MHS04', 'Siti Maryam', 'P', 'JAKARTA', '1995-04-15', 'Teknik Kimia');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`nim`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
